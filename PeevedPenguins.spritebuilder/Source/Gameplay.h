55cfbaebeb31d4e794890d65a6e2c14a2515f4fb
vincentwan <xinwan1992@gmail.com>
2015-02-02 22:20:36 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by 万欣 on 15/2/2.
+//  Copyright (c) 2015年 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
