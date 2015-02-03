c7c2e94c1a99acb9b782ccb99c850f694fb18f9c
vincentwan <xinwan1992@gmail.com>
2015-02-03 00:32:03 -0800

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -19,6 +19,8 @@
 - (void)didLoadFromCCB {
     // tell this scene to accept touches
     self.userInteractionEnabled = TRUE;
+    // visualize physics bodies & joints
+    _physicsNode.debugDraw = TRUE;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
 }
